c722bd6a5503014bf4a91bc91efc4df59c6b4ac8
Clement Barbaza <clement.barbaza@gmail.com>
2021-06-21 16:07:27 +0200

    force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        //
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
